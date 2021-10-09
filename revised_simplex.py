24aac36b1c166ec5eaa0251e0f91634df105fabc
jli0108 <jli0108@terpmail.umd.edu>
2021-10-08 22:50:04 -0400

    fixed parenthesis

--- revised_simplex.py
+++ revised_simplex.py
@@ -11,15 +11,15 @@ maximize : bool = True
 # -- Modify if your problem is in standard or canonical form ----------
 standard : bool = False
 # -- Modify these arrays in the correct format ------------------------
-c : np.ndarray = np.array([[2, 4, 1, 1]])
+c : np.ndarray = np.array([[90, 160, 40, 100]])
 
-A_B : np.ndarray = np.array([[1, 3, 0, 1],
-                [2, 1, 0, 0],
-                [0, 1, 4, 1]])
+A_B : np.ndarray = np.array([[2, 8, 4, 2],
+                [5, 4, 8, 5],
+                [7, 8, 3, 5]])
 
-b : np.ndarray = np.array([[8],
-              [6],
-              [6]])
+b : np.ndarray = np.array([[480],
+              [800],
+              [900]])
 
 assert A_B.shape[0] == b.shape[0]
 assert c.shape[1] == A_B.shape[1]
@@ -91,10 +91,10 @@ def maximize_canonical(c_N, A_N, b):
     index_of_entering_variable = None
     i = 0
     # for first iteration, entering variable is equal to index
-    while i < n and index_of_entering_variable is None:
+    while i < n:# and index_of_entering_variable is None:
         # Uncomment lines above and below to use Bland's rule 
         # (if so, we do not need to check if entering_variable is None)
-        if reduced_c_N[0,i] > 0:
+        if reduced_c_N[0,i] > 0 and (index_of_entering_variable is None or reduced_c_N[0,i] > reduced_c_N[0,index_of_entering_variable]):
         # It should usually be faster if we take the largest coefficient
         #if c_N[i] > 0 and (entering_variable is None or c_N[i] > c_N[entering_variable]):
             index_of_entering_variable = i
@@ -104,11 +104,13 @@ def maximize_canonical(c_N, A_N, b):
 
         reduced_col_of_A_N = np.dot(A_B_inv, A_N[:,index_of_entering_variable])
 
+        print(A_B_inv)
+        print(b_bar[:,0] / reduced_col_of_A_N)
         index_of_leaving_variable = None
         for i in range(m):
             if (reduced_col_of_A_N[i] > 0 and
                 (index_of_leaving_variable is None or
-                b_bar[i,0] * reduced_col_of_A_N[index_of_leaving_variable] < b_bar[index_of_leaving_variable,0]) * reduced_col_of_A_N[i]):
+                b_bar[i,0] * reduced_col_of_A_N[index_of_leaving_variable] < b_bar[index_of_leaving_variable,0] * reduced_col_of_A_N[i])):
 
                 index_of_leaving_variable = i
 
@@ -116,7 +118,9 @@ def maximize_canonical(c_N, A_N, b):
             sys.exit("LP is unbounded")
         entering_variable = nonbasic_variables[index_of_entering_variable]
         leaving_variable = basic_variables[index_of_leaving_variable]
-        #print(f"x{entering_variable+1} enters, x{leaving_variable+1} leaves")
+        print("basis",basic_variables+1)
+        print("nonbasic",nonbasic_variables+1)
+        print(f"x{entering_variable+1} enters, x{leaving_variable+1} leaves")
         # variable enters the basis
         basic_variables[index_of_leaving_variable] = entering_variable
         nonbasic_variables[index_of_entering_variable] = leaving_variable
@@ -127,6 +131,8 @@ def maximize_canonical(c_N, A_N, b):
         # c_B gets updated based on new basic variables
         c_B[0,index_of_leaving_variable] = c[0,entering_variable]
         c_N[0,index_of_entering_variable] = c[0,leaving_variable]
+        #print(A_B)
+        #if (np.linalg.det(A_B) == 0):
 
         A_B_inv = inv(A_B)
         y = np.dot(c_B, A_B_inv)
@@ -135,16 +141,17 @@ def maximize_canonical(c_N, A_N, b):
 
         index_of_entering_variable = None
         i = 0
+        #print(reduced_c_N)
         # for first iteration, entering variable is equal to index
-        while i < n and index_of_entering_variable is None:
+        while i < n:# and index_of_entering_variable is None:
             # Uncomment lines above and below to use Bland's rule 
             # (if so, we do not need to check if entering_variable is None)
-            if reduced_c_N[0,i] > 0:
+            if reduced_c_N[0,i] > 0 and (index_of_entering_variable is None or reduced_c_N[0,i] > reduced_c_N[0,index_of_entering_variable]):
             # It should usually be faster if we take the largest coefficient
             #if c_N[i] > 0 and (entering_variable is None or c_N[i] > c_N[entering_variable]):
                 index_of_entering_variable = i
             i = i + 1
-    
+        #print(index_of_leaving_variable)
     return np.dot(y, b)[0,0], b_bar, basic_variables, y, A_B_inv
 
 #solve_canonical(c, A_B, b, maximize)
